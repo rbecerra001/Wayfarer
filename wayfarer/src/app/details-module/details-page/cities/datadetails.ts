72705e76c4bea19efbcad7053d2f87c16bb3735a
kiran <kiranrakesh@gmail.com>
2021-05-05 14:16:57 -0500

    Add datadetails file

---
+++ wayfarer/src/app/details-module/details-page/cities/datadetails.ts
@@ -0,0 +1,44 @@
+export const CITIES = [
+  {
+    name: 'London',
+    country: 'United Kingdom',
+    image: 'src/app/assets/images/londonTiny.jpeg',
+    posts: [{
+      image: 'src/app/assets/images/londonTiny.jpeg',
+      title: 'Title',
+      description: 'A regal city surrounded by a rush of modern life. London is layered with history, where medieval and Victorian complement a rich and vibrant modern world.'
+    }]
+  },
+
+  {
+    name: 'Sydney',
+    country: 'Australia',
+    image: 'src/app/assets/images/SydneyTiny.jpeg',
+    posts: [{
+      image: 'src/app/assets/images/SydneyTiny.jpeg',
+      title: 'Title',
+      description: 'Sydney, Australia Welcome to Sydney, the vibrant capital of New South Wales. As the official tourism, destinations and events website, this is the place to plan the trip of a lifetime.'
+    }]
+  },
+  {
+    name: 'San Francisco',
+    country: 'United States',
+    image: 'src/app/assets/images/sanFranciscoTiny.jpeg',
+    posts: [{
+      image: 'src/app/assets/images/sanFranciscoTiny.jpeg',
+      title: 'Title',
+      description: 'San Francisco, officially City and County of San Francisco and colloquially known as SF, San Fran, Frisco, or The City, is the cultural, commercial, and financial center of Northern California.'
+    }]
+  },
+  {
+    name: 'Seattle',
+    country: 'United States',
+    image: 'src/app/assets/images/seattleTiny.jpeg',
+    posts: [{
+      image: 'src/app/assets/images/SeattleTiny.jpeg',
+      title: 'Title',
+      description: 'Seattle a seaport city on the West Coast of the United States. It is the seat of King County, Washington.'
+    }]
+  }]
+
+
