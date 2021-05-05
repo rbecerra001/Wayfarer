bd849e955f33b632e34ed5cb5054c6e4889294c4
kiran <kiranrakesh@gmail.com>
2021-05-05 16:17:58 -0500

    Updates

--- wayfarer/src/app/details-module/details-page/details-page.component.ts
+++ wayfarer/src/app/details-module/details-page/details-page.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-details-page',
   templateUrl: './details-page.component.html',
   styleUrls: ['./details-page.component.css']
 })
 export class DetailsPageComponent implements OnInit {
+
 
   constructor() { }
 
