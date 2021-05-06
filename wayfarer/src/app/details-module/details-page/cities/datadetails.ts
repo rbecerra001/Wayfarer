01421c18a4eaf3094026547a11631a3842888e30
kiran <kiranrakesh@gmail.com>
2021-05-06 13:22:23 -0500

    Posts sorted in descending order

--- wayfarer/src/app/details-module/details-page/cities/datadetails.ts
+++ wayfarer/src/app/details-module/details-page/cities/datadetails.ts
@@ -7,18 +7,21 @@ export const CITIES = [
 
     posts: [{
       image: 'assets/images/londonTiny1.jpeg',
-      title: 'Title1',
+      title: 'Regal-city',
+      date: '04-20-2021',
       description: 'A regal city surrounded by a rush of modern life. London is layered with history, where medieval and Victorian complement a rich and vibrant modern world.'
     },
     {
       image: 'assets/images/londonTiny1.jpeg',
-      title: 'Title2',
+      title: 'Heart of UK',
+      date: '04-30-2021',
       description: ' London is among the oldest of the world’s great cities—its history spanning nearly two millennia—and one of the most cosmopolitan. By far Britain’s largest metropolis, it is also the country’s economic, transportation, and cultural centre.'
     },
     {
       image: 'assets/images/londonTiny1.jpeg',
-      title: 'Title3',
-      description: 'London grew from three distinct centres: the walled settlement founded by the Romans on the banks of the Thames in the 1st century CE, today known as the City of London, “the Square Mile.'
+      title: 'Historical background',
+      date: '04-30-2021',
+      description: 'London grew from three distinct centres: the walled settlement founded by the Romans on the banks of the Thames in the 1st century CE, today known as the City of London, “the Square Mile".'
     }
     ]
   },
@@ -29,17 +32,20 @@ export const CITIES = [
     bigImage: 'assets/images/Sydney.jpeg',
     posts: [{
       image: 'assets/images/SydneyTiny.jpeg',
-      title: 'Title1',
+      title: 'Capital-city',
+      date: '03-30-2021',
       description: 'Sydney, Australia Welcome to Sydney, the vibrant capital of New South Wales. As the official tourism, destinations and events website, this is the place to plan the trip of a lifetime.'
       },
       {
         image: 'assets/images/SydneyTiny.jpeg',
-        title: 'Title2',
+        title: 'Best time to visit',
+        date: '05-05-2021',
         description: 'Winter may be the coldest time of the year in Sydney, but temperatures still do not reach under about 10° Celcius, which is still warm by most people’s standards'
       },
       {
         image: 'assets/images/SydneyTiny.jpeg',
-        title: 'Title3',
+        title: 'Cultural diversity',
+        date: '04-30-2021',
         description: ' Sydney’s cultural gems are a celebration of its diversity that will captivate your imagination as well as your soul.'
       }
     ]
@@ -51,17 +57,20 @@ export const CITIES = [
     bigImage: 'assets/images/san fran.jpg',
     posts: [{
       image: 'assets/images/sanFranciscoTiny.jpeg',
-      title: 'Title',
+      title: 'Center of California',
+      date: '04-30-2021',
       description: 'San Francisco, officially City and County of San Francisco and colloquially known as SF, San Fran, Frisco, or The City, is the cultural, commercial, and financial center of Northern California.'
     },
     {
       image: 'assets/images/sanFranciscoTiny.jpeg',
-      title: 'Title',
+      title: 'Good place to visit',
+      date: '12-30-2020',
       description: 'A popular tourist destination, San Francisco is known for its cool summers, fog, steep rolling hills, eclectic mix of architecture, and landmarks, including the Golden Gate Bridge, cable cars, the former Alcatraz Federal Penitentiary, Fisherman\'s Wharf, and its Chinatown district.'
     },
     {
       image: 'assets/images/sanFranciscoTiny.jpeg',
-      title: 'Title',
+      title: 'Variety in food',
+      date: '05-03-2021',
       description: 'San Francisco is as famous for its restaurants and food trends as it is for its Golden Gate Bridge and cable cars. Among the unique or regionally typical foods to be sampled in San Francisco are abalone, Dungeness crab, sand dabs, bay shrimp and crusty sourdough French bread.'
     }
     ]
@@ -73,17 +82,20 @@ export const CITIES = [
     bigImage: 'assets/images/seattle.jpeg',
     posts: [{
       image: 'assets/images/SeattleTiny.jpeg',
-      title: 'Title',
+      title: 'Seaport city',
+      date: '04-30-2021',
       description: 'Seattle a seaport city on the West Coast of the United States. It is the seat of King County, Washington.'
     },
     {
       image: 'assets/images/SeattleTiny.jpeg',
-      title: 'Title',
+      title: 'Beautiful Surroundings',
+      date: '05-01-2021',
       description: 'Seattle, a city on Puget Sound in the Pacific Northwest, is surrounded by water, mountains and evergreen forests, and contains thousands of acres of parkland'
     },
     {
       image: 'assets/images/SeattleTiny.jpeg',
-      title: 'Title',
+      title: 'Best time to visit',
+      date: '04-03-2021',
       description: 'The best time to visit Seattle is from September to October. Summer marks the city\'s high season, meaning room rates rise and availability drops, while cold winter weather can deter even the most avid sightseers.'
     }
     ]
